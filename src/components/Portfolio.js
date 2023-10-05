07ec089b67dd64e710b8d4fd80c8c17505f029a9
Fih <124883284+Firene11@users.noreply.github.com>
2023-10-04 23:59:15 -0300

    realized i need repo links as i was writing my readme file, it's midnight now and i get up at 5am

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -39,31 +39,38 @@ export default function Work() {
   <card style={cardStyle}>
             <center><p style={{marginBottom: '-20px'}}><a href="https://atlantic-glamping-6972d1dfb7e3.herokuapp.com">↓ ATLANTIC GLAMPING ↓</a></p></center><br></br>
             <center><a href="https://atlantic-glamping-6972d1dfb7e3.herokuapp.com"><img src="https://firene11.github.io/Personal-Portfolio/images/AG.jpg" width="100%" height="auto"></img></a></center>
+            <center><a href="https://github.com/Firene11/Atlantic-Glamping">VIEW GITHUB REPO</a></center>
             </card>
+            
 
             <card style={cardStyle}>
             <center><p style={{marginBottom: '-20px'}}><a href="https://firene11.github.io/cosmic-cocktail/">↓ COSMIC COCKTAILS ↓</a></p></center><br></br>
             <a href="https://firene11.github.io/cosmic-cocktail/"><img src="https://firene11.github.io/Personal-Portfolio/images/screenshot.jpg" width="100%" height="auto"></img></a>
+            <center><a href="https://github.com/Firene11/cosmic-cocktail">VIEW GITHUB REPO</a></center>
             </card>
   
             <card style={cardStyle}>
             <center><p style={{marginBottom: '-20px'}}><a href="https://firene11.github.io/Week-01-Challenge/">↓ WEEK ONE CHALLENGE ↓</a></p></center><br></br>
             <a href="https://firene11.github.io/Week-01-Challenge/"><img src="https://firene11.github.io/Personal-Portfolio/images/Week-01-challenge.jpg" width="100%" height="auto"></img></a>
+            <center><a href="https://github.com/Firene11/Week-01-Challenge">VIEW GITHUB REPO</a></center>
             </card>
 
             <card style={cardStyle}>
             <center><p style={{marginBottom: '-20px'}}><a href="https://firene11.github.io/prework-study-guide/">↓ PREWORK STUDY GUIDE ↓</a></p></center><br></br>
             <a href="https://firene11.github.io/prework-study-guide/"><img src="https://firene11.github.io/Personal-Portfolio/images/prework.jpg" width="100%" height="auto"></img></a>
+            <center><a href="https://github.com/Firene11/prework-study-guide">VIEW GITHUB REPO</a></center>
             </card>
 
             <card style={cardStyle}>
             <center><p style={{marginBottom: '-20px'}}><a href="https://https://firene11.github.io/odin-recipes/">↓ ODIN RECIPES ↓</a></p></center><br></br>
             <a href="https://firene11.github.io/odin-recipes/"><img src="https://firene11.github.io/Personal-Portfolio/images/recipes.jpg" width="100%" height="auto"></img></a>
+            <center><a href="https://github.com/Firene11/odin-recipes">VIEW GITHUB REPO</a></center>
             </card>
 
             <card style={cardStyle}>
             <center><p style={{marginBottom: '-20px'}}><a href="https://https://firene11.github.io/Color-State/">↓ COLOR STATE ↓</a></p></center><br></br>
             <a href="https://firene11.github.io/Color-State/"><img src="https://firene11.github.io/Personal-Portfolio/images/color-state.jpg" width="100%" height="auto"></img></a>
+            <center><a href="https://github.com/Firene11/Color-State">VIEW GITHUB REPO</a></center>
             </card>
 
           </section>
